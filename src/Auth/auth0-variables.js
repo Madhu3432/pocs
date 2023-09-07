2c0f2d98bde64821c25ea5ef410c508b80e92673
Madhu gangireddy <madhureddy4204@gmail.com>
2023-09-07 17:41:19 +0530

    poc cloning

---
+++ src/Auth/auth0-variables.js
@@ -0,0 +1,7 @@
+export const AUTH_CONFIG = {
+  domain: 'subdomain.example.com',
+  clientId: '12345asdfasdfasdfasdfasdf1234',
+  baseUrl: 'http://localhost:3000',
+  redirectRoute: '/callback',
+  logoutRoute: '/'
+};
